4f6233943907b2d7a82d143daf0c7950f27981b5
Takis Angelides <takis.angelides@gmail.com>
2023-06-29 16:14:21 +0300

    debug

--- dumpGIF.py
+++ dumpGIF.py
@@ -5,15 +5,14 @@ def dumpGIF(states, filename):
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection = '3d')
-    ax.set_title(f'{0}')
     
     def animate(frame):
         
-        ax.set_title(f'{frame}')
         ax.clear()
         ax.set_axis_off()
+        ax.set_title(f'{frame}')
         
-        return ax.voxels(states[frame%len(states)], edgecolor="k")
+        return ax.voxels(states[frame % len(states)], edgecolor = "k")
     
     ani = animation.FuncAnimation(fig, animate, frames = len(states), interval = 0.2)
     ani.save(filename, writer='pillow')
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from init_state import *
 from rules import *
 from dumpGIF import *
+from dumpGIF_2 import *
 
 def get_configurations(time_steps, initial_state):
 
-    #history[i, :, :] is the i-th timeslice of the evolution
     configurations = np.full(tuple(np.append(time_steps, initial_state.shape)), None, dtype=initial_state.dtype)
 
-    #initialize
     configurations[0] = initial_state
 
     state = initial_state
@@ -18,7 +17,7 @@ def get_configurations(time_steps, initial_state):
     return configurations
         
 
-time_steps = 100
+time_steps = 10
 initial_state = initialize_random_array((4, 4, 4))
 configurations = get_configurations(time_steps, initial_state)
 
