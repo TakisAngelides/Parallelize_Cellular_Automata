10f7f19e7a0da318da513b74bccdcd38cb4192aa
Takis Angelides <takis.angelides@gmail.com>
2023-06-29 18:38:56 +0300

    fix docs

--- neighbours.py
+++ neighbours.py
@@ -1,14 +1,14 @@
 from itertools import product
 import numpy as np
- """
-def count_alive_neighbours(site : np.ndarray, state : np.ndarray) -> int:
+
+def count_alive_neighbours(site: np.ndarray, state: np.ndarray) -> int:
     
-   
-    
+    """
+         
     Inputs:
     
         site : 1-dimensional numpy array of length d - where d is the number of dimensions of the hypercube - specifying the site of a given cell
-    
+
         state : d-dimensional numpy array specifying the state of the cellular automata, the value at each element is boolean-like
 
     product(np.array([0, -1, 1]), repeat = len(site)) is equivalent to [[x, y, z] for x in [0, -1, 1] for y in [0, -1, 1] for z in [0, -1, 1]] for d = 3
@@ -27,18 +27,10 @@ def count_alive_neighbours(site : np.ndarray, state : np.ndarray) -> int:
     Returns:
     
         int : number of alive neighbours
-    
-   
-       
+
     return np.sum([state[element.reshape(1, 1, -1)] for element in ((site+np.array(list(product(np.array([0, -1, 1]), repeat = len(site)))[1:]))%len(state))])
- """
-
-
-
-
-
-
-def count_alive_neighbours(site: np.ndarray, state: np.ndarray) -> int:
+ 
+    """
     
     neighbor_indices = (site + np.array(list(product(np.array([0, -1, 1]), repeat=len(site))))[1:]) % len(state)
     
