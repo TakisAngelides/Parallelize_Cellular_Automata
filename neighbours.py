daeed008bb7670626f3c1ec00cebfceb26d32cfe
volpesc <138021280+volpesc@users.noreply.github.com>
2023-06-29 18:33:48 +0300

    Update neighbours.py

--- neighbours.py
+++ neighbours.py
@@ -1,9 +1,9 @@
 from itertools import product
 import numpy as np
-
+ """
 def count_alive_neighbours(site : np.ndarray, state : np.ndarray) -> int:
     
-    """
+   
     
     Inputs:
     
@@ -28,6 +28,22 @@ def count_alive_neighbours(site : np.ndarray, state : np.ndarray) -> int:
     
         int : number of alive neighbours
     
-    """
+   
        
     return np.sum([state[element.reshape(1, 1, -1)] for element in ((site+np.array(list(product(np.array([0, -1, 1]), repeat = len(site)))[1:]))%len(state))])
+ """
+
+
+
+
+
+
+def count_alive_neighbours(site: np.ndarray, state: np.ndarray) -> int:
+    
+    neighbor_indices = (site + np.array(list(product(np.array([0, -1, 1]), repeat=len(site))))[1:]) % len(state)
+    
+    neighbors = state[tuple(neighbor_indices.T)]
+    
+    num_alive_neighbors = np.sum(neighbors)
+    
+    return num_alive_neighbors
