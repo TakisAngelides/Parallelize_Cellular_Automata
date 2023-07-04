605bd0e57c99dfba19c17c5cb05246fc02f7ba15
Peter Majcen <peter.majcen@pd.infn.it>
2023-07-04 17:31:40 +0200

    surface_growth branch

--- main.py
+++ main.py
@@ -2,23 +2,51 @@ from init_state import *
 from rules import *
 from dumpGIF import *
 
+def get_height(state, height):
+
+
+    for i in range(len(height)):
+
+        if(state[i]):
+            height[i]+=1
+
+    return height
+
+
+def calculate_length(arr):
+    if arr is None:
+        return []  # or any other appropriate default value
+    return len(arr)
+
+
+
 def get_configurations(time_steps, initial_state):
 
     configurations = np.full(tuple(np.append(time_steps, initial_state.shape)), None, dtype=initial_state.dtype)
 
     configurations[0] = initial_state
+    height=np.zeros(initial_state.shape[0])
 
     state = initial_state
     for t in range(1, time_steps):
-        state = apply_rules(state)
+        state = apply_rules_surface_growths(state,height)
         configurations[t] = state
-        
-    return configurations
+        height=get_height(state,height)
         
 
-time_steps = 500
-shape = (50, 50)
-initial_state = initialize_two_glider_octomino(shape)
-configurations = get_configurations(time_steps, initial_state)
 
-dumpGIF(configurations, 'test.gif')
+    return configurations, height
+        
+
+time_steps = 1000
+shape = (100)
+
+#initial_state = initialize_two_glider_octomino(shape)
+initial_state =initialize_array(shape)
+sim = get_configurations(time_steps, initial_state)
+configurations=sim[0]
+height_profile=sim[1]
+
+np.savetxt('output.txt', height_profile, fmt='%d')
+
+#dumpGIF(configurations, 'test.gif')
--- rules.py
+++ rules.py
@@ -1,4 +1,5 @@
 from neighbours import *
+import random as rnd
 
 def apply_rules(state : np.ndarray) -> np.ndarray:
     
@@ -30,6 +31,51 @@ def apply_rules(state : np.ndarray) -> np.ndarray:
             new_state[site_index] = True
         if not current_cell_value and alive == 3:
             new_state[site_index]= True
+    
+                
+    return new_state
+
+def apply_rules_surface_growths(state : np.ndarray,height) -> np.ndarray:
+    
+    """
+
+    Inputs:
+    
+        state : d-dimensional numpy array specifying the state of the cellular automata, the value at each element is boolean-like
+
+    The function trivially iterates over all cells and applies the Clouds 1 rule (see https://softologyblog.wordpress.com/2019/12/28/3d-cellular-automata-3/)
+    
+    # TODO: Parallelize this function
+
+    Returns:
+    
+        new_state : d-dimensional numpy array specifying the state of the cellular automata after 1 time evolution of rules, the value at each element is boolean-like
+    
+    """
+    
+    new_state = np.full(state.shape, False)
+    
+    for site_index, current_cell_value in np.ndenumerate(state):
+        
+        #parameter
+        kappa=0.1
+        rho=0.5
+        
+        #calculate T_i
+        T_i=height[(site_index[0]-1)%len(height)]+height[(site_index[0]+1)%len(height)]-2*height[site_index[0]]
+        #calculate T_max
+        T_max=(-1/kappa)*np.log(rho)
+        if(T_i>=T_max):
+            new_state[site_index] = True
+        else:
+            #calculate p_i
+            p_i=rho*np.exp(kappa*T_i)
+            random_number=rnd.random()
+            if(random_number<p_i):
+                new_state[site_index] = True
+
+
+
     
                 
     return new_state
