abd7e9578248839890f5f0b45e96d757c33df8a8
Takis Angelides <takis.angelides@gmail.com>
2023-07-04 15:49:31 +0300

    test

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 
 @cuda.jit
 def update_state(state, new_state, width, height, configurations, iteration):
-    
-    i, j = cuda.grid(2) # thread id 
+    i, j = cuda.grid(2)
     
     left = (j - 1 + width) % width
     right = (j + 1) % width
@@ -17,27 +16,23 @@ def update_state(state, new_state, width, height, configurations, iteration):
     configurations[i, j, iteration] = new_state[i, j]  # Save the current state in the configurations array
 
 def get_configurations(initial_state, num_iterations, width, height):
-    block_size = (1, 1)  # number of threads within a block
-    grid_size = ((width + block_size[0] - 1) // block_size[0], (height + block_size[1] - 1) // block_size[1]) # number of blocks
+    block_size = (16, 16)
+    grid_size = ((width + block_size[0] - 1) // block_size[0], (height + block_size[1] - 1) // block_size[1])
 
-    state = np.ascontiguousarray(initial_state)  # Create a contiguous copy of the initial state
-    new_state = np.empty_like(state)
-    configurations = np.empty((width, height, num_iterations), dtype=np.uint8)  # Array to store configurations on CPU
+    state_dev = cuda.to_device(initial_state)  # Copy the initial state to the GPU
+    new_state_dev = cuda.device_array_like(state_dev)
+    configurations = np.empty((width, height, num_iterations + 1), dtype=np.uint8)  # Array to store configurations on CPU
+    configurations[:, :, 0] = initial_state  # Store the initial state in the configurations array
     
-    print('test_1')
     configurations_dev = cuda.to_device(configurations)  # Copy configurations array to the GPU
-    print('test_2')
     
     for i in range(num_iterations):
-        update_state[grid_size, block_size](state, new_state, width, height, configurations_dev, i) # on GPU 
-        state, new_state = new_state, state
+        update_state[grid_size, block_size](state_dev, new_state_dev, width, height, configurations_dev, i + 1) # on GPU
     
     cuda.synchronize()  # Ensure all computations on GPU are completed
     
     # Copy the configurations array from GPU to CPU
-    print('test_3')
     configurations = configurations_dev.copy_to_host()
-    print('test_4')
     
     return configurations
 
@@ -46,7 +41,7 @@ width = 100
 height = 100
 
 # Set the number of iterations
-num_iterations = 10  # Increase the number of iterations as per your requirement
+num_iterations = 10
 
 # Create the initial state randomly
 initial_state = np.random.randint(0, 2, size=(width, height), dtype=np.uint8)
@@ -58,6 +53,3 @@ configurations = get_configurations(initial_state, num_iterations, width, height
 final_state = configurations[:, :, -1]
 plt.imshow(final_state, cmap='binary')
 plt.savefig('test.png', bbox_inches='tight')
-
-# Save all the configurations in a separate file or process them as needed
-# configurations[:, :, i] contains the state of the automaton at iteration i
