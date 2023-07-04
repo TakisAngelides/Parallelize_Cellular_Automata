f1e3f844a41d9d66e408f189f723a6d9b8b167d6
Takis Angelides <takis.angelides@gmail.com>
2023-07-04 15:08:38 +0300

    converted to 2d

--- main.py
+++ main.py
@@ -4,28 +4,21 @@ import matplotlib.pyplot as plt
 
 @cuda.jit
 def update_state(state, new_state):
+    i, j = cuda.grid(2)
     
-    """
+    N, M = state.shape
+    left = (j - 1 + M) % M
+    right = (j + 1) % M
+    top = (i - 1 + N) % N
+    bottom = (i + 1) % N
     
-    # Define the kernel to run on GPU
-    # This is called num_iteration number of times and each thread takes care of updating the corresponding index in new_state
-    
-    """
-    
-    i = cuda.grid(1)
-        
-    N = len(state)
-    left = (i - 1 + N) % N
-    right = (i + 1) % N
-            
-    new_state[i] = (state[i-1] + state[i+1]) % 2
+    new_state[i, j] = (state[i, left] + state[i, right] + state[top, j] + state[bottom, j]) % 2
 
 def run_cellular_automaton(initial_state, num_iterations):
-    
     state = initial_state.copy()
     new_state = np.empty_like(state)
-    block_size = 128
-    grid_size = (state.shape[0] + block_size - 1) // block_size
+    block_size = (16, 16)
+    grid_size = ((state.shape[0] + block_size[0] - 1) // block_size[0], (state.shape[1] + block_size[1] - 1) // block_size[1])
 
     for _ in range(num_iterations):
         update_state[grid_size, block_size](state, new_state)
@@ -35,16 +28,18 @@ def run_cellular_automaton(initial_state, num_iterations):
 
 # Set the size of the grid
 width = 100
+height = 100
 
 # Set the number of iterations
 num_iterations = 1
 
 # Create the initial state randomly
-initial_state = np.random.randint(0, 2, size=width, dtype=np.uint8)
+initial_state = np.random.randint(0, 2, size=(width, height), dtype=np.uint8)
 
 # Run the cellular automaton
 final_state = run_cellular_automaton(initial_state, num_iterations)
 
 # Plot the final state
-plt.imshow(final_state.reshape(1, -1), cmap='binary')
-plt.savefig('test.png', bbox_inches = 'tight')
+plt.imshow(final_state, cmap='binary')
+plt.savefig('test.png', bbox_inches='tight')
+
