2444a5fa3a5216a1c2aec008b81a95df0e21b851
Takis Angelides <takis.angelides@gmail.com>
2023-07-04 16:48:33 +0300

    debug

--- main.py
+++ main.py
@@ -25,13 +25,10 @@ def update_state(width, height, configurations, iteration):
             # Any dead cell with exactly three live neighbors becomes a live cell
             configurations[iteration, x, y] = 1
 
-def get_configurations(initial_state, num_iterations, width, height):
+def get_configurations(num_iterations, width, height):
     
     block_size = (1, 1)
     grid_size = ((width + block_size[0] - 1) // block_size[0], (height + block_size[1] - 1) // block_size[1])
-
-    configurations = np.empty((num_iterations + 1, width, height), dtype = int)  # Array to store configurations on CPU
-    configurations[0] = initial_state  # Store the initial state in the configurations array
     
     configurations_dev = cuda.to_device(configurations)  # Copy configurations array to the GPU
     
@@ -43,8 +40,6 @@ def get_configurations(initial_state, num_iterations, width, height):
     # Copy the configurations array from GPU to CPU
     configurations = configurations_dev.copy_to_host()
     
-    return configurations
-
 # Set the size of the grid
 width = 16
 height = 16
@@ -54,7 +49,7 @@ num_iterations = 10
 
 # Create the initial state randomly
 # initial_state = np.random.randint(0, 2, size=(width, height), dtype = np.uint8)
-initial_state = np.zeros((width, height), dtype = int)
+initial_state = np.zeros((width, height), dtype = np.int128)
 initial_state[len(initial_state)//2, (initial_state.shape[0]//2)-1] = 1
 initial_state[len(initial_state)//2, initial_state.shape[0]//2] = 1
 initial_state[len(initial_state)//2, (initial_state.shape[0]//2)+1] = 1
@@ -65,7 +60,9 @@ initial_state[(len(initial_state)//2)-2, (initial_state.shape[0]//2)+2] = 1
 initial_state[(len(initial_state)//2)+1, initial_state.shape[0]//2] = 1
 
 # Run the cellular automaton and get the configurations
-configurations = get_configurations(initial_state, num_iterations, width, height)
+configurations = np.empty((num_iterations + 1, width, height), dtype = np.int128)  # Array to store configurations on CPU
+configurations[0] = initial_state
+configurations = get_configurations(num_iterations, width, height)
 
 print('Now calling to get the gif and save it.')
 start = datetime.now()
