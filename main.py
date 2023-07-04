cb3561fe44a38e237370944022a59129968ee92d
Takis Angelides <takis.angelides@gmail.com>
2023-07-04 14:25:32 +0300

    test

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
-import cupy as cp
+from numba import cuda
+import numpy as np
 import matplotlib.pyplot as plt
 
-@cp.fuse
+@cuda.jit
 def update_state(state, new_state):
-    height, width = 100, 100
+    i = cuda.grid(1)
+    
+    if i > 0 and i < state.shape[0] - 1:
+        new_state[i] = (state[i-1] + state[i] + state[i+1]) % 2
 
-    for i in range(height):
-        for j in range(width):
-            if i == 0 or i == height - 1 or j == 0 or j == width - 1:
-                new_state[i, j] = state[i, j]
-            else:
-                new_state[i, j] = (state[i-1, j-1] + state[i-1, j] + state[i-1, j+1] +
-                                   state[i, j-1] + state[i, j] + state[i, j+1] +
-                                   state[i+1, j-1] + state[i+1, j] + state[i+1, j+1]) // 9
+def run_cellular_automaton(initial_state, num_iterations):
+    state = initial_state.copy()
+    new_state = np.empty_like(state)
+    block_size = 128
+    grid_size = (state.shape[0] + block_size - 1) // block_size
 
-def run_cellular_automaton(initial_state):
-    state = initial_state
-
-    for _ in range(2):
-        new_state = cp.empty_like(state)
-        update_state(state, new_state)
-        state = new_state
+    for _ in range(num_iterations):
+        update_state[grid_size, block_size](state, new_state)
+        state, new_state = new_state, state
 
     return state
 
 # Set the size of the grid
 width = 100
-height = 100
 
 # Set the number of iterations
-num_iterations = 2
+num_iterations = 100
 
 # Create the initial state randomly
-initial_state = cp.random.randint(0, 2, size=(height, width), dtype=cp.int32)
+initial_state = np.random.randint(0, 2, size=width, dtype=np.uint8)
 
 # Run the cellular automaton
-final_state = run_cellular_automaton(initial_state)
-
-# Convert the final state to a NumPy array for visualization
-final_state_np = cp.asnumpy(final_state)
+final_state = run_cellular_automaton(initial_state, num_iterations)
 
 # Plot the final state
-plt.imshow(final_state_np, cmap='binary')
+plt.imshow(final_state.reshape(1, -1), cmap='binary')
 plt.savefig('test.png', bbox_inches = 'tight')
