cc0ad082f04b63f5551d21ed38645b608e10533b
Takis Angelides <takis.angelides@gmail.com>
2023-07-05 13:46:43 +0300

    included srun

--- submit.sh
+++ submit.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+#SBATCH -J main_timed
+#SBATCH -o main_timed.txt
+###SBATCH -e main_timed.err
+#SBATCH -p p100
+#SBATCH --ntasks-per-node=32
+#SBATCH -n 64
+#SBATCH -N 2
 
-#SBATCH --job-name=timing
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --partition=p100
-#SBATCH --error=job.%j.err
-#SBATCH --output=job.%j.out
-
-python main.py
+srun python main.py
+    
