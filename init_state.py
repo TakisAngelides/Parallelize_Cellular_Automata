bab553dd583e12889f78b181cfe4a2ebe6d31e3a
Takis Angelides <takis.angelides@gmail.com>
2023-07-05 12:52:35 +0300

    initial states defined

--- init_state.py
+++ init_state.py
@@ -2,16 +2,29 @@ import numpy as np
 
 def get_initial_state(shape, which_rules):
     
-    if which_rules == '54':
+    if (which_rules == '54'  or  which_rules == '90'):
+        initial_state = np.zeros(shape, dtype = bool)
+        initial_state[len(initial_state)//2] = True
 
-    if which_rules == '90':
+    if (which_rules == 'game_of_life'):
 
-    if which_rules == 'game_of_life':
+        initial_state = np.zeros(shape, dtype = bool)
+        initial_state[len(initial_state)//2, (initial_state.shape[0]//2)-1] = True
+        initial_state[len(initial_state)//2, (initial_state.shape[0]//2)+1] = True
+        initial_state[(len(initial_state)//2)+1, (initial_state.shape[0]//2)+1] = True
+        initial_state[len(initial_state)//2, initial_state.shape[0]//2] = True
+        initial_state[(len(initial_state)//2)-1, (initial_state.shape[0]//2)] = True
 
-    if which_rules == 'tumor_growth':
+    if (which_rules == 'tumor_growth'):
+        initial_state = np.zeros(shape, dtype=bool)
+        initial_state[len(initial_state)//2, initial_state.shape[0]//2] = True
 
-    if which_rules == 'clouds_I':
+    if (which_rules == 'clouds_1'):
+        initial_state = np.zeros(shape, dtype=bool)
+        initial_state = np.random.choice([True,False],shape, p = [0.47,0.53])
 
-    if which_rules == 'builder_II':
-  
+    if (which_rules == 'builder_II'):
+        initial_state = np.zeros(shape, dtype = bool)
+        initial_state[ initial_state.shape[0]//2, initial_state.shape[1]//2, initial_state.shape[2]//2] = True
+
     return initial_state
