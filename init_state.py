465af39107be1ac9322a41572a2c21937a05d315
volpesc <138021280+volpesc@users.noreply.github.com>
2023-07-05 14:56:16 +0300

    Added get_initial_state() with 'which_rules'

--- init_state.py
+++ init_state.py
@@ -36,3 +36,32 @@ def initialize_two_glider_octomino(shape):
     initial_state[(len(initial_state)//2)+1, initial_state.shape[0]//2] = True
     
     return initial_state
+
+def get_initial_state(shape,which_rules):
+    if (which_rules == '54'  or  which_rules == '90'):
+        initial_state = np.zeros(shape, dtype = bool)
+        initial_state[len(initial_state)//2] = True
+
+    if (which_rules == 'game_of_life'):
+
+        initial_state = np.zeros(shape, dtype = bool)
+        initial_state[len(initial_state)//2, (initial_state.shape[0]//2)-1] = True
+        initial_state[len(initial_state)//2, (initial_state.shape[0]//2)+1] = True
+        initial_state[(len(initial_state)//2)+1, (initial_state.shape[0]//2)+1] = True
+        initial_state[len(initial_state)//2, initial_state.shape[0]//2] = True
+        initial_state[(len(initial_state)//2)-1, (initial_state.shape[0]//2)] = True
+
+    if (which_rules == 'tumor_growth'):
+        initial_state = np.zeros(shape, dtype=bool)
+        initial_state[len(initial_state)//2, initial_state.shape[0]//2] = True
+
+    if (which_rules == 'clouds_I'):
+        initial_state = np.zeros(shape, dtype=bool)
+        initial_state = np.random.choice([True,False],shape, p = [0.47,0.53])
+
+    if (which_rules == 'builder_II'):
+        initial_state = np.zeros(shape, dtype = bool)
+        initial_state[ initial_state.shape[0]//2, initial_state.shape[1]//2, initial_state.shape[2]//2] = True
+
+
+    return initial_state
