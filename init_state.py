04f74bea8ac8ca55dbf285ec965985e4938b6889
Takis Angelides <takis.angelides@gmail.com>
2023-07-05 12:55:01 +0300

    return in all ifs

--- init_state.py
+++ init_state.py
@@ -5,6 +5,7 @@ def get_initial_state(shape, which_rules):
     if (which_rules == '54'  or  which_rules == '90'):
         initial_state = np.zeros(shape, dtype = bool)
         initial_state[len(initial_state)//2] = True
+        return initial_state
 
     if (which_rules == 'game_of_life'):
 
@@ -14,17 +15,20 @@ def get_initial_state(shape, which_rules):
         initial_state[(len(initial_state)//2)+1, (initial_state.shape[0]//2)+1] = True
         initial_state[len(initial_state)//2, initial_state.shape[0]//2] = True
         initial_state[(len(initial_state)//2)-1, (initial_state.shape[0]//2)] = True
+        return initial_state
 
     if (which_rules == 'tumor_growth'):
         initial_state = np.zeros(shape, dtype=bool)
         initial_state[len(initial_state)//2, initial_state.shape[0]//2] = True
+        return initial_state
 
     if (which_rules == 'clouds_1'):
         initial_state = np.zeros(shape, dtype=bool)
         initial_state = np.random.choice([True,False],shape, p = [0.47,0.53])
+        return initial_state
 
     if (which_rules == 'builder_II'):
         initial_state = np.zeros(shape, dtype = bool)
         initial_state[ initial_state.shape[0]//2, initial_state.shape[1]//2, initial_state.shape[2]//2] = True
 
-    return initial_state
+        return initial_state
