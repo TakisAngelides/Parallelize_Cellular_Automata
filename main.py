1fa60c6c4d353a807c25d06f871150c67663150b
Takis Angelides <takis.angelides@gmail.com>
2023-07-05 13:39:30 +0300

    pc

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ def get_configurations(items):
     duration = end - start
     
     with open(f'Timing_Results/main_timed/{d}_{time_steps}_{N}_{trial_num}_{which_rules}.txt', 'w') as f:
-        f.write(f'{duration}')
+        f.write(f'{duration.total_seconds()}')
 
 trial_num = 5
-time_steps_list = [10, 100, 1000]
-N_list = [4, 8, 16, 32, 64, 128]
+time_steps_list = [10, 20, 30, 40, 50]
+N_list = [16, 32, 64, 128]
 which_rules_list = ['54', '90', 'game_of_life', 'tumor_growth', 'clouds_I', 'builder_II']
 process_items = []
 
@@ -55,9 +55,9 @@ for i in range(trial_num):
                 
                 initial_state = get_initial_state(shape, which_rules)  
                 
-                process_items.append([time_steps, initial_state, which_rules, trial_num])
+                process_items.append([time_steps, initial_state, which_rules, i])
 
-pool = multiprocessing.Pool(processes = 16)
+pool = multiprocessing.Pool(processes = 64)
 results = pool.map(get_configurations, process_items)       
 pool.close()
 pool.join() 
