5900836ceae6f7d9e7471b302c6d576d53aa6abb
volpesc <138021280+volpesc@users.noreply.github.com>
2023-07-05 15:39:13 +0300

    Update get_initial_state with dimension restriction

--- init_state.py
+++ init_state.py
@@ -39,12 +39,12 @@ def initialize_two_glider_octomino(shape):
     return initial_state
 
 def get_initial_state(shape,which_rules):
-    if (which_rules == '54'  or  which_rules == '90'):
+    if ((which_rules == '54'  or  which_rules == '90') and len(shape) == 1):
         initial_state = np.zeros(shape, dtype = bool)
         initial_state[len(initial_state)//2] = True
         return initial_state
 
-    if (which_rules == 'game_of_life'):
+    if ((which_rules == 'game_of_life') and len(shape) == 2)  :
 
         initial_state = np.zeros(shape, dtype = bool)
         initial_state[len(initial_state)//2, (initial_state.shape[0]//2)-1] = True
@@ -54,17 +54,17 @@ def get_initial_state(shape,which_rules):
         initial_state[(len(initial_state)//2)-1, (initial_state.shape[0]//2)] = True
         return initial_state
 
-    if (which_rules == 'tumor_growth'):
+    if ((which_rules == 'tumor_growth') and len(shape) == 2):
         initial_state = np.zeros(shape, dtype=bool)
         initial_state[len(initial_state)//2, initial_state.shape[0]//2] = True
         return initial_state
 
-    if (which_rules == 'clouds_I'):
+    if ((which_rules == 'clouds_I') and len(shape) == 3):
         initial_state = np.zeros(shape, dtype=bool)
         initial_state = np.random.choice([True,False],shape, p = [0.47,0.53])
         return initial_state
 
-    if (which_rules == 'builder_II'):
+    if ((which_rules == 'builder_II') and len(shape) == 3):
         initial_state = np.zeros(shape, dtype = bool)
         initial_state[ initial_state.shape[0]//2, initial_state.shape[1]//2, initial_state.shape[2]//2] = True
         return initial_state
