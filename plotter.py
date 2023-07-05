c98e989846df533ad17a66c2015cec7d69ca4a27
Takis Angelides <takis.angelides@gmail.com>
2023-07-05 18:30:42 +0300

    debug

--- plotter.py
+++ plotter.py
@@ -21,10 +21,15 @@ for element in os.listdir('Timing_Results/main_timed'):
     df_tmp = pd.DataFrame([data_tmp], columns = columns)
     df = pd.concat([df, df_tmp], ignore_index=True)
 
-d = df.groupby(['d', 'time', 'N', 'rules'], as_index = False)['duration'].mean()
-d = d.rename(columns = {'duration' : 'average_duration'})
+print(df)
+df = df.groupby(['d', 'time', 'N', 'rules'], as_index = False)['duration']
+print(df)
+df = df.mean()
+print(df)
+df = df.rename(columns = {'duration' : 'average_duration'})
+print(df)
 
-d_tmp = d[(d.N == 64) & (d.d == 3) & (d.rules == 'clouds')]
+d_tmp = df[(df.N == 64) & (df.d == 3) & (df.rules == 'clouds')]
 
 x = d_tmp.time
 y = d_tmp.average_duration
