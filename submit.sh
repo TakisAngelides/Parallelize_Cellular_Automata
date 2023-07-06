6beaf186d691dc389783d011466584c99be85e19
Takis Angelides <takis.angelides@gmail.com>
2023-07-06 14:42:48 +0300

    delete neighbours, rules, add plotter, submit

---
+++ submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -J njit_parallel_timed
+#SBATCH -o njit_parallel_timed.txt
+###SBATCH -e njit_parallel_timed.err
+#SBATCH -p p100
+#SBATCH --ntasks-per-node=32
+#SBATCH -n 64
+#SBATCH -N 2
+
+srun python main.py
+    
